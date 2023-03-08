e8b4f57bbe4d27ddadb56553a23aa31c4a6f4d38
irkham <irkham.nazmi@gmail.com>
2023-03-08 10:35:40 +0700

    Second Commit

--- engine/rule/database.php
+++ engine/rule/database.php
@@ -1,36 +1,37 @@
 <?php
-/**
-* Database rule file Uinsu-Web-Framework
-*/
 
 /**
-* Database server (cth : localhost)
-*/
-$dbServer = 'localhost';
+ * Database rule file Uinsu-Web-Framework
+ */
 
 /**
-* Database user
-*/
-$dbUser = 'root';
+ * Database server (cth : localhost)
+ */
+$dbServer = 'sql309.epizy.com';
 
 /**
-* Database password
-*/
+ * Database user
+ */
+$dbUser = 'epiz_33747214';
+
+/**
+ * Database password
+ */
 $dbPassword = '';
 
 /**
-* Database name
-*/
-$dbName = 'db_laundry';
+ * Database name
+ */
+$dbName = 'epiz_33747214_db_loundry';
 
 /**
-* Database driver
-*/
+ * Database driver
+ */
 $dbDriver = 'mysqli';
 
 /**
-* Query builder
-*/
+ * Query builder
+ */
 
 $insert = 'INSERT INTO ';
 $select = 'SELECT ';
@@ -38,16 +39,16 @@ $delete = 'DELETE FROM ';
 $update = 'UPDATE ';
 
 /**
-* Define to public
-*/
+ * Define to public
+ */
 
-define('DB_DRIVER',$dbDriver);
-define('DB_SERVER',$dbServer);
-define('DB_USER',$dbUser);
-define('DB_PASSWORD',$dbPassword);
-define('DB_NAME',$dbName);
+define('DB_DRIVER', $dbDriver);
+define('DB_SERVER', $dbServer);
+define('DB_USER', $dbUser);
+define('DB_PASSWORD', $dbPassword);
+define('DB_NAME', $dbName);
 
 define('DB_INSERT', $insert);
-define('DB_SELECT',$select);
-define('DB_UPDATE',$update);
-define('DB_DELETE',$delete);
+define('DB_SELECT', $select);
+define('DB_UPDATE', $update);
+define('DB_DELETE', $delete);
